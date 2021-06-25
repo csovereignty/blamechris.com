28a83b00ec2ce99b126e9ec7d3e469e2b93db17c
csovereignty <christopher.bowenx@gmail.com>
2021-06-25 00:12:43 -0700

    Not sure what's wrong, but something is!

--- server1.js
+++ server1.js
@@ -1,19 +1,21 @@
-const https = require("https");
-  fs = require("fs");
+const fs = require('fs');
+
 const options = {
-    key: fs.readFileSync("/srv/www/keys/my-site-key.pem"),
-    cert: fs.readFileSync("/srv/www/keys/chain.pem")
+  cert: fs.readFileSync('cert/certificate.crt'),
+  ca: fs.readFileSync('cert/ca_bundle.crt'),
+  key: fs.readFileSync('cert/private.key')
 };
 
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const httpsPort = process.env.PORT || 3030;
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port);
-https.createServer(options, app).listen(port);
-console.log('Server started at http://localhost:' + port);
+httpsPort.createServer(options, app).listen(httpsPort);
+//console.log('Server started at http://localhost:' + port);
